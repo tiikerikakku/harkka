1929bfa90a1891d8aa77125bd955c94f3391c153
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2025-04-15 17:40:23 +0300

    test for collection removal feature

--- src/tests/collectionrepo_test.py
+++ src/tests/collectionrepo_test.py
@@ -20,3 +20,16 @@ class CollectionrepoTest(TestCase):
         e.add_to_collection(uid, mid)
 
         self.assertEqual(e.get_user_collection_formatted(uid), ['happy end [1]'])
+
+    def test_can_remove_movie_from_collection(self):
+        q = UserRepository(self._c)
+        w = MovieRepository(self._c)
+        e = CollectionRepository(self._c)
+
+        uid = q.create_user('jessica')
+        mids = [w.create_movie('stormskärs maja', ''), w.create_movie('lilja 4-ever', '')]
+        cids = [e.add_to_collection(uid, mids[0]), e.add_to_collection(uid, mids[1])]
+
+        e.remove_from_collection(cids[0])
+
+        self.assertEqual(e.get_user_collection_formatted(uid), ['lilja 4-ever [2]'])
