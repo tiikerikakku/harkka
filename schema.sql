8c9698139b96204aab31b79fb51165bf1c20b0a3
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2025-04-15 14:14:26 +0300

    schema updated

--- schema.sql
+++ schema.sql
@@ -4,17 +4,20 @@ drop table if exists users;
 
 create table users (
   id integer primary key,
-  user text unique not null
+  user text unique not null,
+  check(user != '')
 );
 
 create table movies (
   id integer primary key,
   movie text unique not null,
-  info text
+  info text,
+  check(movie != '')
 );
 
 create table collected (
   id integer primary key,
   user integer references users not null,
-  movie integer references movies not null
+  movie integer references movies not null,
+  unique(user, movie)
 );
